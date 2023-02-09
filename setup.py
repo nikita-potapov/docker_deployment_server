e8a6d05fa8777e3ce6af2ed863e6e5eb22b8315c
Nikita Potapov <47923521+nikita-potapov@users.noreply.github.com>
2023-02-09 20:51:51 +0400

    addded setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='ci_example',
+    version='0.0.1',
+    description="Training with GitHub Actions",
+    author='dementevda',
+    install_requires=['Flask', 'docker'],
+    include_package_data=True,
+    keywords=[
+        'ci', 'github actions', 'flask', 'docker'
+    ],
+    entry_points={
+        'console_scripts': [
+            'ci_example = main:main']},
+)
